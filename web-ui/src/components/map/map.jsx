5c3f774819728b8e515d9f0936293386391e638f
James Redding <jdr161@case.edu>
2023-12-11 16:58:56 -0500

    add bounds, min zoom

--- web-ui/src/components/map/map.jsx
+++ web-ui/src/components/map/map.jsx
@@ -5,26 +5,29 @@ import {
     CircleMarker,
     Popup,
 } from "react-leaflet";
+import L from "leaflet";
 import { useState, useMemo } from "react";
 import "leaflet/dist/leaflet.css";
 import Info from "../info";
 import Legend from "../legend"
 import styles from "./map.module.css"
 
+var southWest = L.latLng(-85, -180)
+var northEast = L.latLng(85, 180)
+const bounds = L.latLngBounds(southWest, northEast);
 
+const countryStyle = {
+    fillColor: "red",
+    fillOpacity: 1,
+    color: "black",
+    weight: 2,
+    fillOpacity: 0.7,
+};
 
 // with help from https://github.com/CodingWith-Adam/geoJson-map-with-react-leaflet/blob/master/src/components/MyMap.jsx#L27
 
 function Map({ mapData }) {
     const [selected, setSelected] = useState({})
-
-    const countryStyle = {
-        fillColor: "red",
-        fillOpacity: 1,
-        color: "black",
-        weight: 2,
-        fillOpacity: 0.7,
-    };
 
     const logTest = (e) => {
         console.log(e.target)
@@ -45,7 +48,7 @@ function Map({ mapData }) {
     }
 
     const resetHighlight = (e) => {
-        var layer = e.target; 
+        var layer = e.target;
         layer.setStyle(countryStyle);
         setSelected({})
     }
@@ -62,22 +65,20 @@ function Map({ mapData }) {
         });
     };
 
-    const geoJSON = useMemo(() => <GeoJSON style={countryStyle} data={mapData.features} onEachFeature={onEachCountry}/>, [] );
-    const info = useMemo(() => <Info/>, []);
+    // Because selected updates the state, we need to make sure all of these components do not render
+    // on each change of selected.
+    const tileLayer = useMemo(() => <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />, [])
+    const geoJSON = useMemo(() => <GeoJSON style={countryStyle} data={mapData.features} onEachFeature={onEachCountry} />, []);
+    const info = useMemo(() => <Info />, []);
 
     return (
         <div className={styles.map}>
             <div>
-                <MapContainer center={[40.609787846393196, 20.7890265133657]} zoom={5}>
-                    
-                    <TileLayer
-                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    { geoJSON }
-                    { info }
-                    <Legend selected={selected}/>
-
+                <MapContainer center={[0, 0]} zoom={3} maxBounds={bounds} minZoom={2}>
+                    {tileLayer}
+                    {geoJSON}
+                    {info}
+                    <Legend selected={selected} />
                 </MapContainer>
             </div>
         </div>
