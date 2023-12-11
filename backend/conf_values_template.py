40bd01c49125c807ecfd85a967f2fbb3d293d519
matthewsturm <mjs453@case.edu>
2023-12-11 16:26:17 -0500

    .

---
+++ backend/conf_values_template.py
@@ -0,0 +1,20 @@
+## Replace the relevant values and rename to "conf_values.py"
+
+REQUEST_URL='CSV URL'
+PATH_TO_DIR='Path to Directory where CSV is to reside (Usually this directory)'
+CSV_NAME='Name of CSV File'
+
+#Database
+DB_USER='Database Username'
+DB_PASS='Database Password'
+DB_HOST='Database Hostname'
+DB_PORT='Database Port'
+DB_NAME='Database Name/Identifier'
+DB_TABLES=['Table Name 1', 'Table Name 2', 'Table Name 3', 'Table Name 4'] #leave in brackets
+DB_SCHEMA='Schema Name'
+
+#Table Attributes
+COUNTRY_ATTRIBUTES = ['value1', 'value2', 'value3'] #leave in brackets
+CASES_ATTRIBUTES = ['value1', 'value2', 'value3'] #leave in brackets
+DEATHS_ATTRIBUTES = ['value1', 'value2', 'value3'] #leave in brackets
+VACCINATIONS_ATTRIBUTES = ['value1', 'value2', 'value3'] #leave in brackets
