684f65dae552e3ec21b3617b3e6853a7fb7c2ff8
James Redding <jdr161@case.edu>
2023-12-11 21:54:21 -0500

    db queries with prisma working (waiting on DB property updates)

--- web-ui/src/pages/api/getDataByDate.js
+++ web-ui/src/pages/api/getDataByDate.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
     // Check for API password and correct request syntax
     if (!(req.method === "GET")){
         res.status(405).json({ message: `expected method: GET, received: ${req.method}` })
@@ -8,9 +8,22 @@ export default function handler(req, res) {
         res.status(401).json({ message: 'wrong password, bozo' })
     } else if (!req.body.dataType) {
         res.status(400).json({ message: 'need dataType in body' })
-    } else{
+    } else if (!req.body.date){
+        res.status(400).json({ message: 'need date in body' })
+    } else {
         // See which data type is needed (cases, deaths, or vaccinations)
         if (req.body.dataType === "cases"){
+            // const groupUsers = await prisma.cases.groupBy({
+            //     by: ['iso_code'],
+            //     where: {
+            //         date: {
+            //             contains: 'prisma.io', GREATER THAN? LESS THAN? req.body.date
+            //         },
+            //     },
+            //     _sum: {
+            //         new_cases: true,
+            //     },
+            // })
             res.status(200).json({ name: 'John Doe' })
         } else if (req.body.dataType === "deaths"){
             res.status(200).json({ name: 'John Doe' })
--- web-ui/src/pages/api/getLatestData.js
+++ web-ui/src/pages/api/getLatestData.js
@@ -1,6 +1,8 @@
-export default function handler(req, res) {
+import prisma from '../../lib/prisma'
+
+export default async function handler(req, res) {
     // Check for API password and correct request syntax
-    if (!(req.method === "GET")){
+    if (!(req.method === "GET")) {
         res.status(405).json({ message: `expected method: GET, received: ${req.method}` })
     } else if (!req.body.password) {
         res.status(400).json({ message: 'no password provided, bozo' })
@@ -8,13 +10,34 @@ export default function handler(req, res) {
         res.status(401).json({ message: 'wrong password, bozo' })
     } else if (!req.body.dataType) {
         res.status(400).json({ message: 'need dataType in body' })
-    } else{
+    } else {
         // See which data type is needed (cases, deaths, or vaccinations)
-        if (req.body.dataType === "cases"){
+        if (req.body.dataType === "cases") {
+            const test = await prisma.cases.findUnique({
+                where: {
+                    iso_code_date: {
+                        iso_code: "AFG",
+                        date: "2020-03-30",
+                    },
+                },
+            })
+            return res.json(test)
+            // const countryCases = await prisma.cases.groupBy({
+            //     by: ['iso_code'],
+            //     where: {
+            //         date: {
+            //             lt: req.body.date,
+            //         },
+            //     },
+            //     _sum: {
+            //         new_cases: true,
+            //     },
+            // })
+            // return res.json(countryCases)
             res.status(200).json({ name: 'John Doe' })
-        } else if (req.body.dataType === "deaths"){
+        } else if (req.body.dataType === "deaths") {
             res.status(200).json({ name: 'John Doe' })
-        } else if (req.body.dataType === "vaccinations"){
+        } else if (req.body.dataType === "vaccinations") {
             res.status(200).json({ name: 'John Doe' })
         } else {
             res.status(400).json({ message: 'dataType not recognized' })
