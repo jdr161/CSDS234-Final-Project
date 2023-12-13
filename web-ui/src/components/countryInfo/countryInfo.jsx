ff500ce6ff1e692f57cefc3ce65069198d3f5189
James Redding <jdr161@case.edu>
2023-12-12 21:58:15 -0500

    add "no data available for this location" to locations with no data

--- web-ui/src/components/countryInfo/countryInfo.jsx
+++ web-ui/src/components/countryInfo/countryInfo.jsx
@@ -1,23 +1,21 @@
 import styles from "./countryInfo.module.css";
 
 function Legend({selected}) {
-    var noData = false
-    if(selected.count === -1){
-        noData = true
-    }
-
     const displayFullInfo = () => {
         let rowArr = [2]
-
         rowArr[0] = (<div key="nameRow" className={styles.row}>{selected.name}</div>)
-        rowArr[1] = (<div key="countRow" className={styles.row}>{selected.count}</div>)
+
+        if (selected.count === -1){
+            rowArr[1] = (<div key="countRow" className={styles.row}>No data available for this location</div>)
+        } else {
+            rowArr[1] = (<div key="countRow" className={styles.row}>{selected.count}</div>)
+        }
         return rowArr
     }
 
     return (
         <div className={styles.hoverInfo}>
             { selected.name != null ? (displayFullInfo()) : (<div className={styles.row}>No country Selected</div>)}
-            {/* {selected.ADMIN != null ? (<div className={styles.hoverInfo}> {selected.ADMIN} </div>) : (<div className={styles.hoverInfo}>nothing selected</div>)} */}
         </div>
     )
 }
