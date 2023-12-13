5552cd5ee1f8c9d9db4cbef8af4b72a0459c7172
James Redding <jdr161@case.edu>
2023-12-12 19:49:07 -0500

    get rid of contintent sums that were skewing data

--- web-ui/src/lib/reduceData.js
+++ web-ui/src/lib/reduceData.js
@@ -2,7 +2,7 @@ export default function reduceData(arr){
     if(!Array.isArray(arr)){
         return arr;
     } else {
-        const reduced = arr.reduce((a, v) => ({ ...a, [v["iso_code"]]: v._sum.new_cases}), {}) 
+        const reduced = arr.filter((obj) => obj.iso_code.substring(0,4) !== "OWID").reduce((a, v) => ({ ...a, [v["iso_code"]]: v._sum.new_cases}), {})
         return reduced
     }
 }
